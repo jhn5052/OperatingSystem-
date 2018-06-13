383ef858883574ddcc292bd443e547d0b94a83b8
Han minji <36077160+jhn5052@users.noreply.github.com>
2018-06-13 10:04:36 +0900

    Create pipeline.h

---
+++ pipeline.h
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NUMMEMORY 65536 /* maximum number of data words in memory */
+#define NUMREGS 8 /* number of machine registers */
+
+#define ADD 0
+#define NOR 1
+#define LW 2
+#define SW 3
+#define BEQ 4
+#define JALR 5 /* JALR will not implemented for this project */
+#define HALT 6
+#define NOOP 7
+
+#define MAXLINELENGTH 10
+#define NOOPINSTRUCTION 0x1c00000
+
+typedef struct IFIDStruct {
+	int instr;
+	int pcPlus1;
+} IFIDType;
+
+typedef struct IDEXStruct {
+	int instr;
+	int pcPlus1;
+	int readRegA;
+	int readRegB;
+	int offset;
+} IDEXType;
+
+typedef struct EXMEMStruct {
+	int instr;
+	int branchTarget;
+	int aluResult;
+	int readRegB;
+} EXMEMType;
+
+typedef struct MEMWBStruct {
+	int instr;
+	int writeData;
+} MEMWBType;
+
+typedef struct WBENDStruct {
+	int instr;
+	int writeData;
+} WBENDType;
+
+typedef struct stateStruct {
+	int pc;
+	int instrMem[NUMMEMORY];
+	int dataMem[NUMMEMORY];
+	int reg[NUMREGS];
+	int numMemory;
+	IFIDType IFID;
+	IDEXType IDEX;
+	EXMEMType EXMEM;
+	MEMWBType MEMWB;
+	WBENDType WBEND;
+	int cycles; /* number of cycles run so far */
+} stateType;
+
+void printState(stateType *);
+void printInstruction(int);
+int field0(int);
+int field1(int);
+int field2(int);
+int opcode(int);
